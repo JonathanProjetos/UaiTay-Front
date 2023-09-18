746f76c8184e82b73350af84fea577985329f93a
Jonathan Santos <jonathan01347@gmail.com>
2023-09-18 20:02:46 -0300

    feat(request)Added fetch API

--- src/api/request.js
+++ src/api/request.js
@@ -1,14 +1,30 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:`https://calculadora-uaitay-production.up.railway.app/`
+  // baseURL:`https://calculadora-uaitay-production.up.railway.app/`
+  baseURL:`http://localhost:3001/`
 });
-
 
 export const requestMenuProducts = async () => {
   const { data } = await api.get('menu')
-  // console.log(data);
   return data;
+}
+
+export const requestLogin = async (email, password) => {
+  try {
+    const { data } = await api.post('login', {
+      email,
+      password
+    })
+  
+    console.log(data);
+  
+    return data;
+
+  } catch (err) {
+    return err;
+  }
+
 }
 
 export default api;
