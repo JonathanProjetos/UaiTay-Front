a83f07227fa858dd7f7fa0c3b8e346b99e412561
Jonathan Santos <jonathan01347@gmail.com>
2023-09-20 22:35:25 -0300

    chore(Settings)directory change

---
+++ src/app/pages/settings.jsx
@@ -0,0 +1,39 @@
+"use client"
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation';
+import NewProduct from '../../components/NewProduct'
+import Logout from '../../components/Logout';
+import Box from '@mui/material/Box'
+import { verifyToken } from '../../api/request'
+import { toast } from 'react-toastify'
+
+
+function Settings() {
+  const router = useRouter();
+
+  const auth = async () => {
+    try {
+      await verifyToken()
+    } catch (error) {
+      toast.error('Você não tem autorização para acessar essa página', {
+        position: 'bottom-center',
+        autoClose: 4000,
+      })
+      router.push('/')
+      
+    }
+  }
+
+  useEffect(() => {
+    auth()
+  }, [])
+
+  return (
+    <Box>
+      <Logout/>
+      <NewProduct/>
+    </Box>
+  )
+}
+
+export default Settings
