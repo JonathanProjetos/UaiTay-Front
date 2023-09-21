308afc98fe51d1918a531894c6fde6cc096a29d7
Jonathan Santos <jonathan01347@gmail.com>
2023-09-20 22:37:40 -0300

    feat(Logout)create component logout.

---
+++ src/components/Logout.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useRouter } from 'next/navigation';
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import ButtonBase from '@mui/material/ButtonBase';
+
+function Logout() {
+  const router = useRouter();
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        width: '100vw',
+        marginBottom: '2vh',
+        backgroundColor: '#1976d2',
+      }}
+    >
+      <Box>
+        <Typography
+          sx={{
+            color: 'white',
+            fontWeight: 'bold',
+            fontSize: '4vh',
+            marginLeft: '3vw',
+          }}
+        >
+          DashBoard
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          color: 'white',
+          fontWeight: 'bold',
+          // fontSize: '4vh',
+          marginRight: '3vw',
+        }}
+      >
+        <ButtonBase
+          onClick={() => router.push('/')}
+        >
+          <ExitToAppIcon
+            sx={{
+            fontSize: '4vh',
+            }} 
+          />
+        </ButtonBase>
+      </Box>
+    </Box>
+  )
+}
+
+export default Logout
