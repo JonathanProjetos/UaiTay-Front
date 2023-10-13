2ca87ea4a86be62ed42a6b89d8c1aea6bcccfaae
Jonathan Santos <jonathan01347@gmail.com>
2023-10-13 15:25:26 -0300

    fix(request)changed end point

--- src/api/request.js
+++ src/api/request.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   withCredentials: true,
-  // baseURL:`https://calculadora-uaitay-production.up.railway.app`,
-  baseURL:`http://localhost:3001/`,
+  baseURL:`https://calculadora-uaitay-production.up.railway.app`,
+  // baseURL:`http://localhost:3001/`,
 });
 
 export const requestLogin = async (email, password) => {
