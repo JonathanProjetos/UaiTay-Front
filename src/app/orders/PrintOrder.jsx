99da284e4d5adcce6c44b6b6f3da1657324112d2
Jonathan Santos <jonathan01347@gmail.com>
2023-10-26 20:45:28 -0300

    feat(PrintOrder)added component for order printer

---
+++ src/app/orders/PrintOrder.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+function PrintOrder({ orderData }) {
+  const { date,hours,order, total } = orderData
+  const handlePrint = () => {
+    const printWindow = window.open('', '', 'width=400,height=600'); 
+    printWindow.document.open();
+    printWindow.document.write('<html><head><title>Conteúdo para Impressão</title></head><body>');
+    printWindow.document.write('<div>-------- UaiTay Comida Chinesa --------</div><br>');
+    printWindow.document.write(`<div>---------------------------------------</div>`);
+    printWindow.document.write(`<div>CNPJ: 40.429.040/0001-78</div><br>`);
+    printWindow.document.write(`<div>Endereço: Rua 42 N°358</div><br>`);
+    printWindow.document.write(`<div>Bairro: Tropical</div><br>`);
+    printWindow.document.write(`<div>Cidade: Contagem</div><br>`);
+    printWindow.document.write(`<div>Cep: 32072440</div><br>`);
+    printWindow.document.write(`<div>Telefone: (31) 9 9985-6780</div><br>`);
+    printWindow.document.write(`<div>Data:${date}</div><br>`);
+    printWindow.document.write(`<div>Hora:${hours}</div>`);
+    printWindow.document.write(`<div>---------------------------------------</div><br>`);
+    printWindow.document.write(`<div>----------Descrição do pedido---------</div><br>`);
+    order && order.map((data) => (
+      printWindow.document.write(`<div>1X - ${data.name} - R$:${data.price.toFixed(2).split('.').join(',')}</div><br>`)
+    ))
+    printWindow.document.write(`<div>---------------------------------------</div><br>`);
+    printWindow.document.write(`<div>Total: R$:${total.toFixed(2).split('.').join(',')}</div><br>`);
+    printWindow.document.write(`<div>---------------------------------------</div><br>`);
+    printWindow.document.write(`<div>Obrigado pela preferência.</div>`);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print()
+  };
+
+  return (
+    <div>
+      <Button 
+        size="small"
+        onClick={() => handlePrint()}
+      >
+        Imprimir
+      </Button>
+    </div>
+  ) 
+}
+
+export default PrintOrder
