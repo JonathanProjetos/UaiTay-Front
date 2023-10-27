cb83cbd41b4f78d5b3dfda93a0f0b52869aef038
Jonathan Santos <jonathan01347@gmail.com>
2023-10-26 22:24:22 -0300

    fix:(files)changer settings for printer

--- src/app/orders/PrintOrder.jsx
+++ src/app/orders/PrintOrder.jsx
@@ -6,7 +6,7 @@ function PrintOrder({ orderData }) {
   const handlePrint = () => {
     const printWindow = window.open('', '', 'width=400'); 
     printWindow.document.open();
-    printWindow.document.write('<html><head><title>Conteúdo para Impressão</title></head><body style="margin: 0; font-size: 6px; font-family: Arial, sans-serif;">');
+    printWindow.document.write('<html><head><title>Conteúdo para Impressão</title></head><body style="margin: 0; font-size: 4px; font-family: Arial, sans-serif;">');
     printWindow.document.write('<div>-------- UaiTay Comida Chinesa --------</div><br>');
     printWindow.document.write(`<div>---------------------------------------</div>`);
     printWindow.document.write(`<div>CNPJ: 40.429.040/0001-78</div><br>`);
@@ -18,7 +18,7 @@ function PrintOrder({ orderData }) {
     printWindow.document.write(`<div>Data:${date}</div><br>`);
     printWindow.document.write(`<div>Hora:${hours}</div>`);
     printWindow.document.write(`<div>---------------------------------------</div><br>`);
-    printWindow.document.write(`<div>----------Descrição do pedido---------</div><br>`);
+    printWindow.document.write(`<div>--------Descrição do pedido---------</div><br>`);
     order && order.map((data) => (
       printWindow.document.write(`<div>1X - ${data.name} - R$:${data.price.toFixed(2).split('.').join(',')}</div><br>`)
     ))
