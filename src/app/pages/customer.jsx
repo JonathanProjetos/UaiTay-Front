33c67450e94af1974bd2c7f39553fb194c85b911
Jonathan Santos <jonathan01347@gmail.com>
2023-10-30 22:30:36 -0300

    feat(customer)rendering customer page

---
+++ src/app/pages/customer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CustomerData from '../../app/customer/CustomerData'
+
+function customer() {
+  return (
+    <div>
+      <CustomerData/>
+    </div>
+  )
+}
+
+export default customer
