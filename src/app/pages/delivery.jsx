334371e19f28a26e9d34bdfb29018cdcbaf15791
Jonathan Santos <jonathan01347@gmail.com>
2023-11-04 16:06:21 -0300

    feat(delivery)rendering the data page for delivery.

---
+++ src/app/pages/delivery.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Delivery from '../delivery/DeliveryData'
+
+function Delivery() {
+  return (
+    <div>
+      <Delivery/>
+    </div>
+  )
+}
+
+export default Delivery
