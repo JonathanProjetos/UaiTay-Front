e83b9e58aa4497d509f05c203c0807a247e510ed
Jonathan Santos <jonathan01347@gmail.com>
2023-11-19 15:31:21 -0300

    feat(Listorders)added button form redirect to home.

--- src/app/orders/ListOrders.jsx
+++ src/app/orders/ListOrders.jsx
@@ -1,14 +1,17 @@
 "use client"
 import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation';
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import ButtonBase from '@mui/material/ButtonBase';
 import { requestOrders } from '../../api/request'
 import CardOrder from './CardOrder'
 
 function ListOrders() {
   const [data, setData] = useState([])
 
-  console.log(data);
+  const  router = useRouter();
 
   const getAllOrders = async () => {
     const response = await requestOrders()
@@ -22,21 +25,35 @@ function ListOrders() {
 
   return (
     <Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          height: '8vh',
+          alignItems: 'center',
+          color: 'white',
+          backgroundColor: '#1976d2',
+          marginBottom: '2vh',
+        }}
+      >
         <Typography 
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            height: '8vh',
-            alignItems: 'center',
-            fontSize: '4vh',
-            color:'white',
             fontWeight: 'bold',
-            backgroundColor: '#1976d2',
-            marginBottom: '2vh',
+            fontSize: '4vh',
           }}
         >
           Lista de pedidos
         </Typography>
+        <ButtonBase
+          onClick={() => router.push('/')}
+        >
+          <ExitToAppIcon
+            sx={{
+            fontSize: '4vh',
+            }} 
+          />
+        </ButtonBase>
+        </Box>
       <Box
         sx={{
           display: 'flex',
