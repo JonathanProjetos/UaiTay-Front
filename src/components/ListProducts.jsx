e44870b2591cd0710d6821c6cfdbd0c4f587fd6e
Jonathan Santos <jonathan01347@gmail.com>
2025-03-11 16:05:27 -0300

    refactor: Melhora a exibição da lista de produtos e atualiza a estrutura do componente

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -5,66 +5,73 @@ import Button from '@mui/material/Button'
 import context from '@/context/Context'
 import defaultList from '@/util/defaultList'
 
-function ListProducts({products}) {
+function ListProducts({ products }) {
   const { listProducts, setListProducts } = useContext(context);
-
-  if (!products.length) {
-    alert('Não foi possivel carregar os produtos do back-end');
-  }
 
   const listOfproducts = products.length ? products : defaultList;
   return (
     <Box
       sx={{
         display: 'flex',
-        alignItems: 'end',
-        flexDirection: 'row',
+        alignItems: 'start',
+        flexDirection: 'column',
+        justifyContent: 'center',
       }}
     >
+      {products.length > 0 ? (
+        <Typography variant="h7" sx={{ marginLeft: '20px' }}>
+          Informações da base de dados {new Date().toLocaleDateString('pt-BR')}.
+        </Typography>
+      ) : (
+        <Typography variant="h7" sx={{ marginLeft: '20px' }}>
+          Última atualização 11/03/2025.
+        </Typography>
+      )}
+
       <Box
         sx={{
-        display: 'flex',
-        justifyContent: 'flex-start',
-        marginRight: '30vw',
-        flexWrap: 'wrap',
-        '& > :not(style)': {
-          m: 2,
-          width: 150,
-          height: 150,
-        },
+          display: 'flex',
+          justifyContent: 'flex-start',
+          marginRight: '30vw',
+          flexWrap: 'wrap',
+          '& > :not(style)': {
+            m: 2,
+            width: 150,
+            height: 150,
+          },
         }}
       >
         {
           listOfproducts.map((data) => (
-          <Box key={data._id} sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            border: '1px solid black',
-            width: '100px',
-            height: '100px',
-            borderRadius: '5px',
-          }}
-          >
-            <Button
-              variant="contained"
-              sx={{
-                width: '100%',
-                height: '100%',
-                backgroundColor: products.length > 0 ? '#1976d2' : '#a83535',
-              }}
-              onClick={() => setListProducts([...listProducts, data]) }
+            <Box key={data.name} sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              border: '1px solid black',
+              width: '100px',
+              height: '100px',
+              borderRadius: '5px',
+            }}
             >
-              <Typography 
-                variant="h7" 
-                component="div"
+              <Button
+                variant="contained"
                 sx={{
-                  fontWeight: 'bold',
+                  width: '100%',
+                  height: '100%',
+                  backgroundColor: products.length > 0 ? '#1976d2' : '#a83535',
                 }}
+                onClick={() => setListProducts([...listProducts, data])}
               >
-                {data.name}
-              </Typography>
-            </Button>
-          </Box>
+                <Typography
+                  variant="h7"
+                  component="div"
+                  sx={{
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {data.name}
+                </Typography>
+              </Button>
+            </Box>
           ))
         }
       </Box>
